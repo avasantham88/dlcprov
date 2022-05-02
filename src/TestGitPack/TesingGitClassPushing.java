f8539c22e63d25f86100785fb253cb9d689d4956
dlcprov <avasantham88@gmail.com>
2022-05-02 17:41:31 +0530

    Iam commited the testing git

---
+++ src/TestGitPack/TesingGitClassPushing.java
@@ -0,0 +1,15 @@
+package TestGitPack;
+
+public class TesingGitClassPushing {
+	
+	int a=100;
+	String name="dhana";
+	
+	public static void main(String[] args) {
+	int b=1000;
+	System.out.println("I am the test Git to push into github"+ b);
+	}
+	
+ 
+
+}
