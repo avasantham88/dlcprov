66b87ceda86e9eb27295c6a1044546d3509e2e1a
dlcprov <avasantham88@gmail.com>
2022-05-02 17:50:33 +0530

    second update

---
+++ src/TestGitPack/SecondClasstoPushGit.java
@@ -0,0 +1,8 @@
+package TestGitPack;
+
+public class SecondClasstoPushGit {
+	public static void main(String[] args) {
+		System.out.println("I am boss");
+	}
+
+}
--- src/TestGitPack/TesingGitClassPushing.java
+++ src/TestGitPack/TesingGitClassPushing.java
@@ -1,15 +1,24 @@
 package TestGitPack;
 
+import java.nio.channels.ShutdownChannelGroupException;
+
 public class TesingGitClassPushing {
 	
 	int a=100;
 	String name="dhana";
 	
+	private void showoff() {
+	System.out.println(" Iam the class member function ");
+	}
 	public static void main(String[] args) {
 	int b=1000;
 	System.out.println("I am the test Git to push into github"+ b);
+	TesingGitClassPushing tg=new TesingGitClassPushing();
+	tg.showoff();
+			
 	}
 	
  
+	
 
 }
